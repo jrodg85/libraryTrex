93a5fae65ef155910f3490140a54afa6b8a48a37
jrodg85 <lordbuba85@gmail.com>
2022-06-06 13:42:47 +0200

    cambio nombre

---
+++ app/src/main/java/es/libraryTrex/ActividadInterfaz.java
@@ -0,0 +1,13 @@
+package es.libraryTrex;
+
+public interface ActividadInterfaz {
+
+  public String getCiudad();
+
+  public Coordinador getCoordinador();
+
+  void setCiudad(String ciudad);
+
+  void setCoordinador(Coordinador coordinador);
+
+}
